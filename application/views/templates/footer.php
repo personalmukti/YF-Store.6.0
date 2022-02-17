a26c75395a448f7f06c7bd6df4ccb53032665623
personalmukti <69576871+personalmukti@users.noreply.github.com>
2022-02-17 09:53:56 +0700

    Update footer.php
    Update new footer, adding tawk.to chat widget and add new bootstrap star rating js.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -108,11 +108,12 @@ $rekening = $this->db->get('rekening');
         </div>
         </footer>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="<?= base_url(); ?>assets/js/jquery.countdown.min.js"></script>
+    <!-- Bootstrap Star Rating JS -->
+    <script src='<?= base_url() ?>assets/vendor/bootstrap-star-rating/js/star-rating.min.js' type='text/javascript'></script>
     <script>
 
         $("i.icon-search-navbar").on('click', function(){
@@ -172,4 +173,18 @@ $rekening = $this->db->get('rekening');
         })
 
     </script>
+
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6200f2e79bd1f31184db4f02/1fr9r9n1i';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </html>
